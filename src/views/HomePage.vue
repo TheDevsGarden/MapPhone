628194bc75b50afde4abefdf596833f9c4fb92b4
TheDevsGarden <iteratebelyaev@gmail.com>
2024-08-03 21:43:26 -0400

    maps working console logging phone numbers

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -8,7 +8,7 @@
         <ion-title>MapPhone Directory</ion-title>
       </ion-toolbar>
       <ion-toolbar>
-        <ion-searchbar placeholder="Search entity or phone number" :debounce="300" @ionInput="handleSearch()"></ion-searchbar>
+        <ion-searchbar placeholder="Search entity or phone number" :debounce="1000" @ionInput="handleSearch($event)"></ion-searchbar>
       </ion-toolbar>
     </ion-header>
 
@@ -27,7 +27,7 @@
       </ion-text> -->
 
       <!-- Gonna put a Map -->
-      <div tappable id="map" style="width: 100%; height: 16rem; border: 1px solid grey"></div>
+      <div tappable id="map" style="width: 100%; height: 45%; border: 1px solid grey"></div>
       <!-- Gonna put a list that refreshes alphabetically based on Radius -->
     </ion-content>
   </ion-page>
@@ -37,12 +37,35 @@
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonList, IonItem, IonLabel, IonButtons, IonMenuButton, IonSpinner, IonText, IonButton } from "@ionic/vue";
+import { Geolocation } from "@capacitor/geolocation";
 
 const router = useRouter();
 const searchResults = ref([]);
 const loading = ref(false);
+const searchTerm = ref("");
 
-const handleSearch = async () => {};
+const handleSearch = async (event: CustomEvent) => {
+  try {
+    loading.value = true;
+    searchTerm.value = event.detail.value;
+    searchResults.value = [];
+
+    const request = {
+      textQuery: searchTerm.value,
+      fields: ["displayName", "location", "internationalPhoneNumber"],
+      isOpenNow: true,
+      maxResultCount: 3,
+      useStrictTypeFiltering: false,
+    };
+
+    await findPlaces(request);
+  } catch (error) {
+    console.error("Error during search:", error);
+    searchResults.value = [];
+  } finally {
+    loading.value = false;
+  }
+};
 
 const viewentityDetails = (entityId: string) => {
   //router.push({ name: "entityDetails", params: { id: entityId } });
@@ -56,7 +79,17 @@ let center: google.maps.LatLngLiteral;
 async function initMap() {
   const { Map } = (await google.maps.importLibrary("maps")) as google.maps.MapsLibrary;
 
-  center = { lat: 37.4161493, lng: -122.0812166 };
+  const getCurrentPosition = async () => {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      center = { lat: coordinates.coords.latitude, lng: coordinates.coords.longitude };
+    } catch (error) {
+      console.error("Error getting location", error);
+      center = { lat: 37.4161493, lng: -122.0812166 };
+    }
+  };
+
+  await getCurrentPosition();
   map = new Map(document.getElementById("map") as HTMLElement, {
     center: center,
     zoom: 11,
@@ -65,48 +98,43 @@ async function initMap() {
   findPlaces();
 }
 
-async function findPlaces() {
+async function findPlaces(request: google.maps.places.TextSearchRequest) {
   const { Place } = (await google.maps.importLibrary("places")) as google.maps.PlacesLibrary;
   const { AdvancedMarkerElement } = (await google.maps.importLibrary("marker")) as google.maps.MarkerLibrary;
-  const request = {
-    textQuery: "Tacos in Mountain View",
-    fields: ["displayName", "location", "businessStatus"],
-    includedType: "restaurant",
-    locationBias: { lat: 37.4161493, lng: -122.0812166 },
-    isOpenNow: true,
-    language: "en-US",
-    maxResultCount: 8,
-    minRating: 3.2,
-    region: "us",
-    useStrictTypeFiltering: false,
-  };
 
-  const { places } = await Place.searchByText(request);
+  try {
+    const { places } = await Place.searchByText(request);
 
-  if (places.length) {
-    console.log(places);
+    if (places.length) {
+      const { LatLngBounds } = (await google.maps.importLibrary("core")) as google.maps.CoreLibrary;
+      const bounds = new LatLngBounds();
+      console.log("latlongbnds");
 
-    const { LatLngBounds } = (await google.maps.importLibrary("core")) as google.maps.CoreLibrary;
-    const bounds = new LatLngBounds();
+      // Clear existing markers (if any)
+      //map.clear();
 
-    places.forEach((place) => {
-      const markerView = new AdvancedMarkerElement({
-        map,
-        position: place.location,
-        title: place.displayName,
+      places.forEach((place) => {
+        const markerView = new AdvancedMarkerElement({
+          map,
+          position: place.location,
+          title: place.displayName,
+        });
+
+        if (place.location) {
+          bounds.extend(place.location);
+        }
+        console.log(place.internationalPhoneNumber);
       });
 
-      if (place.location) {
-        bounds.extend(place.location);
+      if (!bounds.isEmpty()) {
+        map.fitBounds(bounds);
+        console.log("fitbounds");
       }
-      console.log(place);
-    });
-
-    if (!bounds.isEmpty()) {
-      map.fitBounds(bounds);
+    } else {
+      console.log("No results");
     }
-  } else {
-    console.log("No results");
+  } catch (error) {
+    console.error("Error searching for places:", error);
   }
 }
 
